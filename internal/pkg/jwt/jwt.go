276f2f81229238efa12e8d2b2eeac475dc4f5b45
Mmx233 <admin@mmxblog.com>
2023-04-22 01:34:04 +0800

    feat: jwt 包

---
+++ internal/pkg/jwt/jwt.go
@@ -0,0 +1,41 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt/v4"
+	"time"
+)
+
+type Claims struct {
+	jwt.RegisteredClaims
+}
+
+func New(key []byte, validate time.Duration) Generator {
+	return Generator{
+		key:      key,
+		validate: validate,
+	}
+}
+
+type Generator struct {
+	key      []byte
+	validate time.Duration
+}
+
+func (a Generator) NewToken() (string, error) {
+	return jwt.NewWithClaims(jwt.SigningMethodES256, &Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(a.validate)),
+		},
+	}).SignedString(a.key)
+}
+
+func (a Generator) VerifyToken(token string) (bool, error) {
+	t, e := jwt.ParseWithClaims(token, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return a.key, nil
+	})
+	if e != nil {
+		return false, e
+	}
+	return t.Valid, nil
+}
