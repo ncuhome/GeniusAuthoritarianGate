2eb7dddeec0dcd5c75c21e0fc581a2877cf5f518
Mmx233 <admin@mmxblog.com>
2023-04-22 01:12:50 +0800

    feat: 流量转发逻辑

--- cmd/gate/main.go
+++ cmd/gate/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/ncuhome/GeniusAuthoritarianGate/internal/api/gateway"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	if e := gateway.Run(":80"); e != nil {
+		logrus.Fatalln("启动监听失败:", e)
+	}
 }
---
+++ internal/api/gateway/gateway.go
@@ -0,0 +1,26 @@
+package gateway
+
+import (
+	gateway "github.com/Mmx233/Gateway"
+	"github.com/gin-gonic/gin"
+	"github.com/ncuhome/GeniusAuthoritarianGate/internal/global"
+	"github.com/ncuhome/GeniusAuthoritarianGate/tools"
+	"github.com/sirupsen/logrus"
+)
+
+func Run(addr ...string) error {
+	gin.SetMode(gin.ReleaseMode)
+	E := gin.Default()
+
+	E.Use(gateway.Proxy(&gateway.ApiConf{
+		Addr:   global.Config.Addr,
+		Client: tools.Http.Client,
+		ErrorHandler: func(c *gin.Context, e error) {
+			logrus.Errorln("request backend failed:", e)
+			c.AbortWithStatus(502)
+		},
+		AllowAll: true,
+	}))
+
+	return E.Run(addr...)
+}
