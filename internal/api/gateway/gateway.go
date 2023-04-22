4a13fe2c8e5814c3c9f8a93a7f7a70cd4061b1fd
Mmx233 <admin@mmxblog.com>
2023-04-22 16:15:31 +0800

    improve: 尝试优化代理性能

--- internal/api/gateway/gateway.go
+++ internal/api/gateway/gateway.go
@@ -1,12 +1,13 @@
 package gateway
 
 import (
-	gateway "github.com/Mmx233/Gateway"
+	gateway "github.com/Mmx233/Gateway/v2"
 	"github.com/gin-gonic/gin"
 	"github.com/ncuhome/GeniusAuthoritarianGate/internal/api/middlewares"
 	"github.com/ncuhome/GeniusAuthoritarianGate/internal/global"
 	"github.com/ncuhome/GeniusAuthoritarianGate/internal/util"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func Run(addr ...string) error {
@@ -16,11 +17,10 @@ func Run(addr ...string) error {
 	E.Use(middlewares.Auth(global.Config.JwtKey))
 
 	E.Use(gateway.Proxy(&gateway.ApiConf{
-		Addr:   global.Config.Addr,
-		Client: util.Http.Client,
-		ErrorHandler: func(c *gin.Context, e error) {
+		Addr:      global.Config.Addr,
+		Transport: util.Http.Client.Transport,
+		ErrorHandler: func(_ http.ResponseWriter, _ *http.Request, e error) {
 			log.Errorln("request backend failed:", e)
-			c.AbortWithStatus(502)
 		},
 		AllowAll: true,
 	}))
