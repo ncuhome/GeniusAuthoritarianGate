362ba1cf7a379f967d7f5eb2058f8ce88ae8ae46
Mmx233 <admin@mmxblog.com>
2023-11-30 17:28:47 +0800

    fix: 修正 aes 加密解密逻辑

--- internal/pkg/aes/aes.go
+++ internal/pkg/aes/aes.go
@@ -1,13 +1,13 @@
 package aes
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/rand"
-	"fmt"
+	"encoding/base64"
+	"errors"
 	"github.com/ncuhome/GeniusAuthoritarianGate/internal/global"
 	log "github.com/sirupsen/logrus"
-	"io"
 )
 
 var key []byte
@@ -19,38 +19,67 @@ func init() {
 	key = []byte(global.Config.AesKey)
 }
 
+func pkcs7Padding(data []byte, blockSize int) []byte {
+	padding := blockSize - (len(data) % blockSize)
+	padText := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(data, padText...)
+}
+func pkcs7UnPadding(data []byte) ([]byte, error) {
+	if len(data) == 0 {
+		return nil, errors.New("input data is empty")
+	}
+
+	padding := int(data[len(data)-1])
+	if padding > len(data) || padding == 0 {
+		return nil, errors.New("invalid padding")
+	}
+
+	for i := len(data) - padding; i < len(data); i++ {
+		if data[i] != byte(padding) {
+			return nil, errors.New("invalid padding")
+		}
+	}
+
+	return data[:len(data)-padding], nil
+}
+
 func Encrypt(str []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
 
-	ciphertext := make([]byte, aes.BlockSize+len(str))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
+	str = pkcs7Padding(str, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key[:block.BlockSize()])
+	encrypted := make([]byte, len(str))
+	blockMode.CryptBlocks(encrypted, str)
 
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(ciphertext[aes.BlockSize:], str)
+	output := make([]byte, base64.URLEncoding.EncodedLen(len(encrypted)))
+	base64.URLEncoding.Encode(output, encrypted)
 
-	return ciphertext, nil
+	return output, nil
 }
 
 func Decrypt(str []byte) ([]byte, error) {
+	ciphertext := make([]byte, base64.URLEncoding.DecodedLen(len(str)))
+	n, err := base64.URLEncoding.Decode(ciphertext, str)
+	if err != nil {
+		return nil, err
+	}
+	ciphertext = ciphertext[:n]
+
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
-	if len(str) < aes.BlockSize {
-		return nil, fmt.Errorf("ciphertext too short")
+
+	if len(ciphertext) < block.BlockSize() || len(ciphertext)%block.BlockSize() != 0 {
+		return nil, errors.New("invalid ciphertext")
 	}
 
-	iv := str[:aes.BlockSize]
-	str = str[aes.BlockSize:]
+	blockMode := cipher.NewCBCDecrypter(block, key[:block.BlockSize()])
+	decrypted := make([]byte, len(ciphertext))
+	blockMode.CryptBlocks(decrypted, ciphertext)
 
-	mode := cipher.NewCBCDecrypter(block, iv)
-	mode.CryptBlocks(str, str)
-
-	return str, nil
+	return pkcs7UnPadding(decrypted)
 }
