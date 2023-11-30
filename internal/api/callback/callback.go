ae05862405ea0bc234b05a67371e1d7674d5596d
Mmx233 <admin@mmxblog.com>
2023-11-30 18:05:30 +0800

    feat: 添加 api callback 包

---
+++ internal/api/callback/callback.go
@@ -0,0 +1,36 @@
+package callback
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+type Msg struct {
+	Code       uint8       `json:"code"`
+	Data       interface{} `json:"data,omitempty"`
+	Msg        string      `json:"msg,omitempty"`
+	HttpStatus int         `json:"-"`
+}
+
+func Error(c *gin.Context, msg *Msg, args ...any) {
+	log.Debugln(msg.Msg, args)
+	c.JSON(msg.HttpStatus, msg)
+	c.Abort()
+}
+
+func ErrorWithTip(c *gin.Context, msg *Msg, tip any, args ...any) {
+	tipMsg := *msg
+	tipMsg.Msg = fmt.Sprint(tip)
+	Error(c, &tipMsg, args...)
+}
+
+func Success(c *gin.Context, data interface{}) {
+	c.JSON(200, &Msg{
+		Data: data,
+	})
+}
+
+func Default(c *gin.Context) {
+	Success(c, nil)
+}
